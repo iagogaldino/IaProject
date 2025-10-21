033b38c846663061f8520be010ddc212b4216f59
Iago Galdino <iago_galdino@hotmail.com>
2025-10-21 18:09:14 -0300

    Remove utility scripts, test files, and log files

--- ia-service/ia-backend/openiafile.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import pdf from "pdf-parse";
-
-const buffer = fs.readFileSync("meuarquivo.pdf");
-
-pdf(buffer).then(data => {
-    console.log(data.text); // todo o texto extraído
-});
